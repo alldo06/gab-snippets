0dc99814fbacecc610dd5cf7cdc9a3e1fa4b24f9
alldo06 <aldovector06@gmail.com>
2024-03-23 15:34:12 +0700

    feat: UI & method to create data and save to db

---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,60 @@
+import { db } from "@/db"
+import { redirect } from "next/navigation";
+
+export default function SnippetCreatePage() {
+
+	async function createSnippet(formData: FormData) {
+		// This need to be server action
+		'use server';
+
+		// Check the user's input
+		const title = formData.get('title') as string
+		const code = formData.get('code') as string
+
+		// Create a new record in the database
+		const snippet = await db.snippet.create({
+			data: {
+				title,
+				code
+			}
+		})
+		console.log(snippet);
+		
+		// Redirect to list/home
+		redirect('/')
+	}
+
+	return (
+    <form action={createSnippet}>
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label htmlFor="title" className="w-12">
+            Title
+          </label>
+          <input
+            id="title"
+            type="text"
+            name="title"
+            className="border rounded p-2 w-full"
+          />
+        </div>
+
+        <div className="flex gap-4">
+          <label htmlFor="code" className="w-12">
+            Code
+          </label>
+          <textarea
+            id="code"
+            name="code"
+            className="border rounded p-2 w-full"
+          />
+        </div>
+
+        <button type="submit" className="rounded p-2 bg-blue-200">
+          Create
+        </button>
+      </div>
+    </form>
+  );
+}
