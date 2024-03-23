eaef7047cd9a0209f94a90b41bc21cb0dbb393d1
alldo06 <aldovector06@gmail.com>
2024-03-23 21:05:44 +0700

    style: homepage & detail snippet page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,30 @@
-import Image from "next/image";
-import { db } from "@/db";
+import Image from 'next/image';
+import Link from 'next/link';
+import { db } from '@/db';
 
 export default async function Home() {
-	const snippets = await db.snippet.findMany()
+  const snippets = await db.snippet.findMany();
 
-	const renderedSnippets = snippets.map(snippet => {
-		return (
-			<div key={snippet.id} className="snippet">{snippet.title}</div>
-		)
-	})
+  const renderedSnippets = snippets.map((snippet) => {
+    return (
+      <Link
+        key={snippet.id}
+        href={`/snippets/${snippet.id}`}
+        className="flex justify-between items-center p-2 border rounded"
+      >
+				<div className="">{snippet.title}</div>
+				<div>View</div>
+      </Link>
+    );
+  });
 
   return (
-		<main className="flex flex-col items-center justify-between p-24">
-			<h1>Home Page</h1>
-			<div className="py-5">{renderedSnippets}</div>
+		<main className="flex flex-col items-center p-12">
+			<div className="flex justify-between m-2 items-center w-full">
+				<h1 className="text-xl font-bold">Snippets</h1>
+				<Link href={`/snippets/new`} className='border p-2 rounded'>New</Link>
+			</div>
+      <div className="py-5 w-full flex flex-col gap-2">{renderedSnippets}</div>
     </main>
   );
 }
--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -18,7 +18,19 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
 		return notFound()
 	}
 
-	return <div className="">
-		{snippet.title}
-	</div>;
+	return (
+		<div className="detail-page">
+    <div className="flex m-4 justify-between items-center">
+      <h1 className="text-xl font-bold">{snippet.title}</h1>
+			<div className="flex gap-2">
+				<button className="p-2 border rounded">Edit</button>
+				<button className="p-2 border rounded">Delete</button>
+			</div>
+			</div>
+			
+			<pre className="p-3 rounded bg-gray-200 borde-gray-200">
+				<code>{ snippet.code }</code>
+			</pre>
+		</div>
+  );
 }
