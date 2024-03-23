086f101a67eee826631778a5c0cc60981b12e2d2
alldo06 <aldovector06@gmail.com>
2024-03-23 21:38:51 +0700

    feat: edit snippet pages with client component & monaco editor

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/db";
 import { notFound } from "next/navigation";
+import Link from "next/link";
 
 interface SnippetShowPageProps {
 	params: {
@@ -23,7 +24,7 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     <div className="flex m-4 justify-between items-center">
       <h1 className="text-xl font-bold">{snippet.title}</h1>
 			<div className="flex gap-2">
-				<button className="p-2 border rounded">Edit</button>
+				<Link href={`/snippets/${snippet.id}/edit`} className="p-2 border rounded">Edit</Link>
 				<button className="p-2 border rounded">Delete</button>
 			</div>
 			</div>
