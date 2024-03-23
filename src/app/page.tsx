774d5bc3d9f4cfc25266f08bd750fa9dd90ddc8c
alldo06 <aldovector06@gmail.com>
2024-03-23 16:01:14 +0700

    feat: fetch and render snippets list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import { db } from "@/db";
 
-export default function Home() {
+export default async function Home() {
+	const snippets = await db.snippet.findMany()
+
+	const renderedSnippets = snippets.map(snippet => {
+		return (
+			<div key={snippet.id} className="snippet">{snippet.title}</div>
+		)
+	})
+
   return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+		<main className="flex flex-col items-center justify-between p-24">
 			<h1>Home Page</h1>
+			<div className="py-5">{renderedSnippets}</div>
     </main>
   );
 }
