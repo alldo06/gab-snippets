a58fe732a1672abde5ebd9da77c052da407d364d
alldo06 <aldovector06@gmail.com>
2024-03-24 15:33:29 +0700

    feat: implement edit action snippet using .bind

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -3,6 +3,7 @@
 import type { Snippet } from '@prisma/client';
 import Editor from '@monaco-editor/react';
 import { useState } from 'react';
+import { editSnippet } from '@/actions';
 
 interface SnippetEditFormProps {
   snippet: Snippet;
@@ -16,6 +17,8 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
 		setCode(value)
 	}
 
+	const editSnippetAction = editSnippet.bind(null, snippet.id, code)
+
   return (
     <div className="">
       <Editor
@@ -27,7 +30,10 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
           minimap: { enabled: false }
 				}}
 				onChange={handleEditorChange}
-      />
+			/>
+			<form action={editSnippetAction}>
+				<button type="submit" className='p-2 border rounded'>Save</button>
+			</form>
     </div>
   );
 }
