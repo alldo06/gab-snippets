ffaf3cd28b09f3e81dcf86c15d45b345f1f1dca6
alldo06 <aldovector06@gmail.com>
2024-03-31 05:32:26 +0700

    feat: add new snippet validation & refactor the server action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,21 +1,54 @@
 'use server';
 
-import { db } from "@/db";
-import { redirect } from "next/navigation";
+import { db } from '@/db';
+import { redirect } from 'next/navigation';
 
 export async function editSnippet(id: number, code: string) {
-	await db.snippet.update({
-		where: { id },
-		data: { code }
-	})
+  await db.snippet.update({
+    where: { id },
+    data: { code }
+  });
 
-	redirect(`/snippets/${id}`)
+  redirect(`/snippets/${id}`);
 }
 
 export async function deleteSnippet(id: number) {
-	await db.snippet.delete({
-		where: { id }
-	})
+  await db.snippet.delete({
+    where: { id }
+  });
 
-	redirect(`/`);
+  redirect(`/`);
+}
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  // Check the user's input
+  const title = formData.get('title');
+  const code = formData.get('code');
+
+  if (typeof title !== 'string' || title.length < 3) {
+    return {
+      message: 'Title must be longer'
+    };
+  }
+
+  if (typeof code !== 'string' || code.length < 10) {
+    return {
+      message: 'Code must be longer'
+    };
+  }
+
+  // Create a new record in the database
+  const snippet = await db.snippet.create({
+    data: {
+      title,
+      code
+    }
+  });
+  console.log(snippet);
+
+  // Redirect to list/home
+  redirect('/');
 }
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,31 +1,15 @@
-import { db } from "@/db"
-import { redirect } from "next/navigation";
+'use client';
+
+import { useFormState } from 'react-dom';
+import * as actions from '@/actions';
 
 export default function SnippetCreatePage() {
+  const [formState, action] = useFormState(actions.createSnippet, {
+    message: ''
+  });
 
-	async function createSnippet(formData: FormData) {
-		// This need to be server action
-		'use server';
-
-		// Check the user's input
-		const title = formData.get('title') as string
-		const code = formData.get('code') as string
-
-		// Create a new record in the database
-		const snippet = await db.snippet.create({
-			data: {
-				title,
-				code
-			}
-		})
-		console.log(snippet);
-		
-		// Redirect to list/home
-		redirect('/')
-	}
-
-	return (
-    <form action={createSnippet}>
+  return (
+    <form action={action}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -50,6 +34,12 @@ export default function SnippetCreatePage() {
             className="border rounded p-2 w-full"
           />
         </div>
+
+        {formState.message ? (
+          <div className="my-2 p-2 bg-red-200 border border-red-400 rounded">
+            {formState.message}
+          </div>
+        ) : null}
 
         <button type="submit" className="rounded p-2 bg-blue-200">
           Create
